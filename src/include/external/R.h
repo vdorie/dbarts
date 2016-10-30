b0f0307579c251b84e3b82b66e8a98c92b83eb4e
Vincent Dorie <vdorie@gmail.com>
2016-10-30 15:26:39 -0400

    R devel (3.4) header compatibility

--- src/include/external/R.h
+++ src/include/external/R.h
@@ -3,20 +3,30 @@
 
 // imports R.h while doing the least to pollute namespaces
 
-// prevents R_ext/Error.h from mapping Rf_error -> error and Rf_warning -> warning
-#define R_NO_REMAP
-
-// R.h includes R_ext/Memory.h, R_ext/Utils.h, and R_ext/RS.h which reference size_t
-// RS.h also defines macros that use functions from string.h, but are not actually
-// referenced in that file
-#define NO_C_HEADERS
-#ifdef __cplusplus
-#  include <cstddef>
-using std::size_t;
-#else
-#  include <stddef.h>
+#include <Rversion.h>
+#ifdef R_VERSION
+#  define __R_MAJOR (R_VERSION >> 16)
+#  define __R_MINOR ((R_VERSION - (__R_MAJOR << 16)) >> 8)
+#  define __R_PATCH ((R_VERSION - (__R_MAJOR << 16)) - (__R_MINOR << 8))
 #endif
 
+// for older versions of R we attempt to no include unnecessary headers,
+// which assists in checking namespace and inclusion correctness
+#if !defined(__R_MAJOR) || (__R_MAJOR < 3 && (!defined(__R_MINOR) || __R_MAJOR != 3 || __R_MINOR < 4))
+// unfortunately, some headers are necessary to include
+#  define NO_C_HEADERS
+#  ifdef __cplusplus
+#    include <cstddef>
+using std::size_t;
+#  else
+#    include <stddef.h>
+#  endif
+#endif
+
+#include <R.h>
+
+// prevents R_ext/Error.h from mapping Rf_error -> error and Rf_warning -> warning
+#define R_NO_REMAP
 #include <R.h>
 
 #undef NO_C_HEADERS
--- src/include/external/Rinternals.h
+++ src/include/external/Rinternals.h
@@ -3,24 +3,32 @@
 
 // imports R.h while doing the least to pollute namespaces
 
-// prevents R_ext/Error.h from mapping Rf_error -> error and Rf_warning -> warning
-#define R_NO_REMAP
-
-// Rinternals.h includes R_ext/Memory.h and R_ext/Utils.h which reference size_t
-// Rinternals.h also references FILE from stdio.h
-#define NO_C_HEADERS
-#ifdef __cplusplus
-#  include <climits>
-#  include <cstddef>
-#  include <cstdio>
-using std::size_t;
-using std::FILE;
-#else
-#  include <limits.h>
-#  include <stddef.h>
-#  include <stdio.h>
+#include <Rversion.h>
+#ifdef R_VERSION
+#  define __R_MAJOR (R_VERSION >> 16)
+#  define __R_MINOR ((R_VERSION - (__R_MAJOR << 16)) >> 8)
+#  define __R_PATCH ((R_VERSION - (__R_MAJOR << 16)) - (__R_MINOR << 8))
 #endif
 
+#if !defined(__R_MAJOR) || (__R_MAJOR < 3 && (!defined(__R_MINOR) || __R_MAJOR != 3 || __R_MINOR < 4))
+// Rinternals.h includes R_ext/Memory.h and R_ext/Utils.h which reference size_t
+// Rinternals.h also references FILE from stdio.h
+#  define NO_C_HEADERS
+#  ifdef __cplusplus
+#    include <climits>
+#    include <cstddef>
+#    include <cstdio>
+using std::size_t;
+using std::FILE;
+#  else
+#    include <limits.h>
+#    include <stddef.h>
+#    include <stdio.h>
+#  endif
+#endif
+
+// prevents R_ext/Error.h from mapping Rf_error -> error and Rf_warning -> warning
+#define R_NO_REMAP
 #include <Rinternals.h>
 
 #undef NO_C_HEADERS
