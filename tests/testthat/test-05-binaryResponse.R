c35a67229f0d9eb60e178455f9fc5a9ec82d8ad5
Vincent Dorie <vdorie@gmail.com>
2019-04-01 22:33:19 -0400

    test suite

--- tests/testthat/test-05-binaryResponse.R
+++ tests/testthat/test-05-binaryResponse.R
@@ -5,7 +5,7 @@ source(system.file("common", "probitData.R", package = "dbarts"))
 test_that("basic probit example passes regression test", {
   n.burn <- 10L
   n.sims <- 100L
-
+  
   set.seed(99)
   bartFit <- bart(y.train = testData$Z, x.train = testData$X, ntree = 50L, ndpost = n.sims, nskip = n.burn,
                   k = 4.5, verbose = FALSE)
@@ -16,10 +16,13 @@ test_that("basic probit example passes regression test", {
 })
 
 test_that("basic probit example with offset regression test", {
+  n.burn <- 10L
+  n.sims <- 100L
+  
   set.seed(99)
   bartFit <- bart(y.train = testData$Z, x.train = testData$X, ntree = 50L, ndpost = n.sims, nskip = n.burn,
                   k = 4.5, binaryOffset = 0.1, verbose = FALSE)
-
+  
   n.sims <- nrow(bartFit$yhat.train)
   
   expect_equal(bartFit$yhat.train[n.sims, 1:5], c(0.157043723005439, 0.649674546901119, 0.392826725618914, 0.510142912804732, -0.27263185358599))
