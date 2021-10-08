ae9bca33aac3cb8b150548cc137116d2369e622e
Vincent Dorie <vdorie@gmail.com>
2021-10-08 16:38:44 -0400

    Version bump

--- src/include/external/io.h
+++ src/include/external/io.h
@@ -2,7 +2,16 @@
 #define EXTERNAL_IO_H
 
 #include <external/R.h>  // R_FlushConsole
+
+#include <Rversion.h>
+
+#if R_VERSION >= R_Version(3, 6, 2)
+#define USE_FC_LEN_T
+#endif
+
 #include <R_ext/Print.h> // Rprintf
+
+#undef USE_FC_LEN_T
 
 // defines a crossplatform NORETURN http://www.open-std.org/jtc1/sc22/wg14/www/docs/n1453.htm
 #if _MSC_VER >= 1310 
