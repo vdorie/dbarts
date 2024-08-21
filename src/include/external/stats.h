72c3e1771af5689fef4c85fdabac58468761686b
Vincent Dorie <vdorie@gmail.com>
2024-08-21 16:03:33 -0400

    Try not remapping math functions

--- src/include/external/stats.h
+++ src/include/external/stats.h
@@ -1,12 +1,19 @@
 #ifndef EXTERNAL_STATS_H
 #define EXTERNAL_STATS_H
 
+#ifndef R_NO_REMAP_RMATH
+#  define UNMAP_R_NO_REMAP_RMATH
+#  define R_NO_REMAP
+#  define R_NO_REMAP_RMATH
+#endif
+
 #include <Rversion.h>
 
 #if R_VERSION >= R_Version(3, 6, 2)
 #define USE_FC_LEN_T
 #endif
 
+#include <R.h>
 #include <Rmath.h> // used to pull in qchisq, et al
 
 #undef USE_FC_LEN_T
@@ -22,8 +29,18 @@ extern "C" {
 #define ext_cumulativeProbabilityOfNormal(_Q_, _MU_, _SIGMA_) Rf_pnorm5((_Q_), (_MU_), (_SIGMA_), 1, 0)
 #define ext_quantileOfNormal(_P_, _MU_, _SIGMA_) Rf_qnorm5((_P_), (_MU_), (_SIGMA_), 1, 0)
 
+extern double Rf_pnorm5(double, double, double, int, int);
+extern double Rf_pchisq(double, double, int, int);
+extern double Rf_qchisq(double, double, int, int);
+
 #ifdef __cplusplus
 }
+#endif
+
+#ifdef UNMAP_R_NO_REMAP_RMATH
+#  undef R_NO_REMAP_RMATH
+#  undef R_NO_REMAP
+#  undef UNMAP_R_NO_REMAP_RMATH
 #endif
 
 #endif // EXTERNAL_STATS_H
