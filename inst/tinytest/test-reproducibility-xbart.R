5ce010eebce8e668e83c78b5329c6dd5bf456583
Vincent Dorie <vdorie@gmail.com>
2026-09-09 03:46:28 -0400

    Replay the xbart seeded-drift snapshot on the reference build

--- inst/tinytest/test-reproducibility-xbart.R
+++ inst/tinytest/test-reproducibility-xbart.R
@@ -57,8 +57,8 @@ res.kf <- apply(xval.kf, 2L, mean)
 res.rs <- apply(xval.rs, 2L, mean)
 
 reference <- list(
-  kf = c(2.4366063501151, 4.5532985484437),
-  rs = c(2.44200830568094, 4.64040529040152)
+  kf = c(2.46359911974021, 4.55694205364568),
+  rs = c(2.48465321388054, 4.45176678548512)
 )
 
 expect_equal(unname(res.kf), reference$kf)
